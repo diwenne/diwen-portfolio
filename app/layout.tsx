280fc757e4d84197286362c9d18ba6ada57afb19
diwenne <diwenh5@icloud.com>
2025-11-04 20:45:41 -0800

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import dynamic from 'next/dynamic'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import { ThemeProvider } from '@/components/theme-provider'
 
 const SplashCursor = dynamic(
